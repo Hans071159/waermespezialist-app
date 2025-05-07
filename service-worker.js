9efd03017482aeefd9a46aff2d88efd0169feff1
Hans071159 <ehmoewes@gmx.de>
2025-05-07 11:05:46 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+
+self.addEventListener('install', function(e) {
+  console.log('Service Worker: Installed');
+  e.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
